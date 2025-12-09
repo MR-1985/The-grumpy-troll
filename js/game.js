7fc50a141a761a0857e67162f93c7d4aa21530c4
Marco Rößler <118630069+MR-1985@users.noreply.github.com>
2025-12-09 13:19:26 +0100

    added MovableObject; deleted old code; added scipt.js to html

--- js/game.js
+++ js/game.js
@@ -1,15 +1,10 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 function init(){
 canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
 
-
-character.src = "./img/png/troll1/troll1-walk.png";
-
-character.onload = () => {
-        ctx.drawImage(character, 20, 50, 100, 90);
-    };
+console.log("My char is",character)
 }
