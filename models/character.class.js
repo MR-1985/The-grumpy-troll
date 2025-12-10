999e7767abbd7d6cdb9b13aa16b91256484b9624
Marco Rößler <118630069+MR-1985@users.noreply.github.com>
2025-12-10 13:51:51 +0100

    added  some chars

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,10 @@
  */
 class Character extends MovableObject {
 
+    constructor() {
+        super().loadImage("img/trolls/png/troll1/Troll_01_1_WALK_000.png");
+    }
+
     jump() {
 
     }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,4 +3,8 @@
  */
 class Chicken extends MovableObject {
   
+     constructor() {
+        super().loadImage("img/trolls/png/troll1/Troll_01_1_WALK_000.png");
+    }
+
 }
